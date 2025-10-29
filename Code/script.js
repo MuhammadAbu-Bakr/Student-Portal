07862adeaafdb5d1a382ae695e87bbf1404859f8
Muhammad Abu-Bakr <abubakrmuhammed075@gmail.com>
2025-10-29 08:30:45 -0700

    Enhance Profile and Registration pages with new headings, improved layout, and added functionality; implement student profile loading and logout features

--- Code/script.js
+++ Code/script.js
@@ -6,5 +6,37 @@ document.getElementById("form").addEventListener('submit',function(e){
     msg.style.marginTop="15px";
     msg.style.fontWeight="bold";
     msg.textContent ="✅ Registration Successful!"
+    msg.style.textAlign="center";
     alert("✅ Registration Successful!");
 })
+
+
+
+
+
+
+// profile
+
+const loadStudent = () =>{
+    const data = localStorage.getItem("studentProfile")
+    return data? JSON.parse(data): null;
+};
+const removeStudent =()=>{
+    localStorage.removeItem("studentProfile");
+};
+
+const student = loadStudent();
+
+if (!student) {
+    alert("❌ Please register first!");
+    window.location.href="Registration.html";
+}else{
+    const infoHTML="<p><strong>Name:</strong>${student.name}</p> <p><strong>Email:</strong>${student.email}</p> <p><strong>Reg No:</strong>${student.regno}</p> <p><strong>Date of Birth:</strong>${student.dob}</p> <p><strong>Department:</strong>${student.department}</p> <p><strong>Semester:</strong>${student.semester}</p>";
+    document.getElementById("profileInfo").innerHTML=infoHTML;
+}
+
+
+document.getElementById("logoutBtn").addEventListener("click", () => {
+    removeStudent();
+    window.location.href="Registration.html";
+});
